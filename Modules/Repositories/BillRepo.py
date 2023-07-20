020b311fbf667396affa697fe77f7456dd1cc7c0
Darus Slah <darusslah@gmail.com>
2023-07-20 19:04:46 -0400

    added microservice for bill

---
+++ Modules/Repositories/BillRepo.py
@@ -0,0 +1,19 @@
+class BillRepo:
+    def __init__(self, session):
+        self.session = session
+
+    def save(self, bill: Bill):
+        self.session.add(bill)
+        self.session.commit()
+        return bill
+
+    def find_by_id(self, bill_id):
+        return self.session.query(Bill).filter(Bill.id == bill_id).first()
+
+    def find_all_bills_by_account_id(self, account_id):
+        return self.session.query(Bill).filter(Bill.account_id == account_id).all()
+
+    def delete(self, bill_id):
+        bill = self.find_by_id(bill_id)
+        if bill:
+            self.session.delete(bill)
