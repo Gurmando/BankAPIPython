2c0ffe7f30a54cd77d80e3712b8d428811d62afa
Mohammed amine ztait <Mztait@wil.yearup.org>
2023-07-23 20:00:54 -0400

    loco

--- Modules/Services/accountS.py
+++ Modules/Services/accountS.py
@@ -1,89 +1,135 @@
-
 from flask import Flask, jsonify, request, abort
+import mysql.connector
 
 app = Flask(__name__)
 
-# Sample account data (to simulate database)
-accounts_data = [
-    {
-        "id": 1,
-        "type": "Savings",
-        "nickname": "Leon's savings account",
-        "rewards": 100,
-        "balance": 1000.0,
-        "customer": "customer1"
-    },
-    {
-        "id": 2,
-        "type": "Checking",
-        "nickname": "John's checking account",
-        "rewards": 50,
-        "balance": 5000.0,
-        "customer": "customer2"
-    }
-]
+# Function to establish a MySQL connection
+def get_mysql_connection():
+    return mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="Lina2011",
+        database="AmineAccountBank"
+    )
 
-# Account types enum
-account_types = ["Savings", "Checking", "Credit"]
+class Account:
+    def __init__(self, account_id, type, nickname, rewards, balance, customer):
+        self.id = account_id
+        self.type = type
+        self.nickname = nickname
+        self.rewards = rewards
+        self.balance = balance
+        self.customer = customer
 
-# Route to get all accounts
-@app.route('/accounts', methods=['GET'])
-def get_all_accounts():
-    return jsonify(accounts_data)
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            data["id"],
+            data["type"],
+            data["nickname"],
+            data["rewards"],
+            data["balance"],
+            data["customer"]
+        )
 
-# Route to get account details by account ID
-@app.route('/accounts/<int:account_id>', methods=['GET'])
-def get_account_by_id(account_id):
-    account = next((acc for acc in accounts_data if acc["id"] == account_id), None)
-    if account:
-        return jsonify(account)
-    else:
-        abort(404)
+    # Route to get all accounts
+    @staticmethod
+    @app.route('/accounts', methods=['GET'])
+    def get_all_accounts():
+        conn = get_mysql_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM accounts")
+        accounts_data = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return jsonify(accounts_data)
 
-# Route to get all accounts for a specific customer
-@app.route('/customers/<string:customer_id>/accounts', methods=['GET'])
-def get_accounts_for_customer(customer_id):
-    customer_accounts = [acc for acc in accounts_data if acc["customer"] == customer_id]
-    return jsonify(customer_accounts)
+    # Route to get account details by account ID
+    @staticmethod
+    @app.route('/accounts/<int:account_id>', methods=['GET'])
+    def get_account_by_id(account_id):
+        conn = get_mysql_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM accounts WHERE id = %s", (account_id,))
+        account_data = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        if account_data:
+            account = Account.from_dict(account_data)
+            return jsonify(account.__dict__)
+        else:
+            abort(404)
 
-# Route to create an account for a specific customer
-@app.route('/customers/<string:customer_id>/accounts', methods=['POST'])
-def create_account_for_customer(customer_id):
-    data = request.get_json()
-    account = {
-        "id": len(accounts_data) + 1,
-        "type": data.get("type"),
-        "nickname": data.get("nickname"),
-        "rewards": data.get("rewards"),
-        "balance": data.get("balance"),
-        "customer": customer_id
-    }
-    accounts_data.append(account)
-    return jsonify({"message": "Account created successfully"}), 201
+    # Route to get all accounts for a specific customer
+    @staticmethod
+    @app.route('/customers/<string:customer_id>/accounts', methods=['GET'])
+    def get_accounts_for_customer(customer_id):
+        conn = get_mysql_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM accounts WHERE customer = %s", (customer_id,))
+        customer_accounts_data = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        customer_accounts = [Account.from_dict(data) for data in customer_accounts_data]
+        return jsonify([acc.__dict__ for acc in customer_accounts])
 
-# Route to update an existing account
-@app.route('/accounts/<int:account_id>', methods=['PUT'])
-def update_account(account_id):
-    data = request.get_json()
-    account = next((acc for acc in accounts_data if acc["id"] == account_id), None)
-    if account:
-        account.update({
-            "type": data.get("type"),
-            "nickname": data.get("nickname"),
-            "rewards": data.get("rewards"),
-            "balance": data.get("balance"),
-        })
-        return jsonify({"message": "Account updated successfully"})
-    else:
-        abort(404)
+    # Route to create an account for a specific customer
+    @staticmethod
+    @app.route('/customers/<string:customer_id>/accounts', methods=['POST'])
+    def create_account_for_customer(customer_id):
+        data = request.get_json()
+        account = Account(
+            None,
+            data.get("type"),
+            data.get("nickname"),
+            data.get("rewards"),
+            data.get("balance"),
+            customer_id
+        )
+        conn = get_mysql_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO accounts (type, nickname, rewards, balance, customer) VALUES (%s, %s, %s, %s, %s)",
+                       (account.type, account.nickname, account.rewards, account.balance, account.customer))
+        conn.commit()
+        account.id = cursor.lastrowid
+        cursor.close()
+        conn.close()
+        return jsonify({"message": "Account created successfully", "account": account.__dict__}), 201
 
-# Route to delete an existing account
-@app.route('/accounts/<int:account_id>', methods=['DELETE'])
-def delete_account(account_id):
+    # Route to update an existing account
+    @staticmethod
+    @app.route('/accounts/<int:account_id>', methods=['PUT'])
+    def update_account(account_id):
+        data = request.get_json()
+        account = Account(
+            account_id,
+            data.get("type"),
+            data.get("nickname"),
+            data.get("rewards"),
+            data.get("balance"),
+            None
+        )
+        conn = get_mysql_connection()
+        cursor = conn.cursor()
+        cursor.execute("UPDATE accounts SET type = %s, nickname = %s, rewards = %s, balance = %s WHERE id = %s",
+                       (account.type, account.nickname, account.rewards, account.balance, account.id))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return jsonify({"message": "Account updated successfully"}), 200
 
-    global accounts_data
-    accounts_data = [acc for acc in accounts_data if acc["id"] != account_id]
-    return jsonify({"message": "Account deleted successfully"})
+    # Route to delete an existing account
+    @staticmethod
+    @app.route('/accounts/<int:account_id>', methods=['DELETE'])
+    def delete_account(account_id):
+        conn = get_mysql_connection()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM accounts WHERE id = %s", (account_id,))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return jsonify({"message": "Account deleted successfully"}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
